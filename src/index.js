fc5224b40d87d1842acb8f26c1e192e797d68ac8
Edwin Narváez <47750859+Patricio18@users.noreply.github.com>
2020-07-29 15:06:01 -0500

    otra config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config({path: '.env'})
 import app from './server'
 
 async function main() {
-    app.listen(app.get('port'), )
+    app.listen(app.get('port'), "127.0.0.1")
     console.log(`Servidor en on : http://localhost:${app.get('port')}/graphql`)
 }
 
